69e4cb506e759429828a27236d08a7ead41b3547
ataur28 <ataurrahman862828@gmail.com>
2023-05-03 17:08:04 +0600

    added footer part

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Banner = () => {
+    return (
+        <Container>
+            <h2>this is banner</h2>
+        </Container>
+    );
+};
+
+export default Banner;
