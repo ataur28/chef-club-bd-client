2b5850bb91491e75994d5711c351b629e6ef195e
ataur28 <ataurrahman862828@gmail.com>
2023-05-04 15:51:19 +0600

    added login and resister form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './Login.css'
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>login</h2>
+        <div className='w-50 mx-auto mt-5 bg-light p-5'>
+            <h3>Please Login</h3>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+                <br />
+                <Form.Text className="text-secondary">
+                    Don't Have an Account? <Link to="/register">Register</Link>
+                </Form.Text>
+                <Form.Text className="text-success">
+
+                </Form.Text>
+                <Form.Text className="text-danger">
+
+                </Form.Text>
+            </Form>
         </div>
     );
 };
